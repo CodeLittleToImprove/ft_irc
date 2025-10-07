170f7262b27e9ca92a7b293a312ae5043d5b3dee
tbui-quo <tbui-quo@student.42wolfsburg.de>
2025-10-07 17:37:45 +0200

    first iteration of working sending message from client to server and print

--- client/client.cpp
+++ client/client.cpp
@@ -18,13 +18,24 @@ int main()
 	serverAddress.sin_addr.s_addr = INADDR_ANY;
 
 	// sending connection request
-	connect(clientSocket, (struct sockaddr*)&serverAddress,
+	connect(clientSocket, (struct sockaddr*)&serverAddress, // connect is counterpart to accept
 			sizeof(serverAddress));
 
 	// sending data
-	const char* message = "Hello, server!";
-	send(clientSocket, message, strlen(message), 0);
-
+	// const char* message = "Hello, server!";
+	// const char* message = ;
+	std::cout << "Connected to server! Type messages to send.\n";
+	std::cout << "(Type '*' to start sending message to server)\n";
+	std::cout << "(Type '#' to quit)\n";
+	std::string message;
+	while (true)
+	{
+		std::cout << "> ";
+		std::getline(std::cin, message);
+		send(clientSocket, message.c_str(), message.size(), 0);
+		if (message.find('#') != std::string::npos)
+			break;
+	}
 	// closing socket
 	close(clientSocket);
 
--- server/server.cpp
+++ server/server.cpp
@@ -5,7 +5,9 @@
 #include <netinet/in.h> // needed for sockaddr_in
 #include <sys/socket.h> // for portability
 #include <unistd.h> // for close
+#include <cstdio> // for perror
 #include <limits>
+#include <stdlib.h>
 
 // this is how a sockaddrr_in looks like
 // struct sockaddr_in
@@ -18,10 +20,8 @@
 
 int main()
 {
-	// int min = std::numeric_limits<float>::min();
-	// std::cout << min << std::endl;
 	int portnbr = 8080;
-	// creating socket
+	// creating server socket
 	int serverSocket = socket(AF_INET, SOCK_STREAM, 0); // input: IPv4 protocol, TCP socket, default
 
 	// specifying the address
@@ -31,25 +31,61 @@ int main()
 	serverAddress.sin_port = htons(portnbr); // converts to network byte order
 	serverAddress.sin_addr.s_addr = INADDR_ANY; // accept connections on any IP
 
-	// binding socket.
+	// binding socket to address
 	bind(serverSocket, (struct sockaddr*)&serverAddress,
 	     sizeof(serverAddress));
 
 	// listening to the assigned socket
-	listen(serverSocket, 5);
+	listen(serverSocket, 5); // n = how many pending connections can be queued
+	std::cout << "Server listening on port " << portnbr << "..." << std::endl;
 
-	// accepting connection request
-	int clientSocket
-		= accept(serverSocket, (struct sockaddr*)&serverAddress, NULL);
+	sockaddr_in clientAddress;
+	socklen_t clientAddressLen = sizeof(clientAddress);
+	// accepting client connection
+	int clientSocket = accept(serverSocket, (struct sockaddr*)&clientAddress, &clientAddressLen);
+	// accepts ands you a new socket FD representing one side of an already-established TCP connection.
 
-	// receiving data
-	char buffer[1024] = {0};
-	recv(clientSocket, buffer, sizeof(buffer), 0);
-	std::cout << "Message from client: " << buffer
-		<< std::endl;
+	if (clientSocket < 0)
+	{
+		perror("accept failed");
+		exit(EXIT_FAILURE);
+	}
 
-	// closing the socket.
+	bool sessionActive = true;
+	char buffer[1024];
+	while (sessionActive) // means here is an accepted connection
+	{
+		memset(buffer, 0, sizeof(buffer));
+		// receiving data from client
+		int bytesReceived = recv(clientSocket, buffer, sizeof(buffer) - 1, 0);
+		if (bytesReceived <= 0)
+		{
+			std::cout << "Client disconnected" << std::endl;
+			break;
+		}
+		for (int i = 0; i < bytesReceived; i++)
+		{
+			char c = buffer[i];
+			if (c == '*')
+			{
+				std::cout << "Client can start to send message" << std::endl;
+				std::cout << "bytesReceived: " << bytesReceived << std::endl;
+				break;
+			}
+
+			else if (c == '#')
+			{
+				std::cout << "Client wants to end the connection" << std::endl;
+				sessionActive = false;
+				break;
+			}
+			// else
+			// 	std::cout << c;
+		}
+		std::cout << "bytesReceived: " << bytesReceived << std::endl;
+		std::cout << buffer << std::endl;
+	}
+	close(clientSocket);
 	close(serverSocket);
-
 	return 0;
 }
