514bbab4c2d5e475a2318fd0b2654b8e20c8fe5b
Thang Bui Quoc <tbuiloves42@gmail.com>
2025-10-13 16:29:35 +0200

    fix interation of a continuous connection where every message ends with \r\n

--- client/client.cpp
+++ client/client.cpp
@@ -5,9 +5,13 @@
 #include <netinet/in.h>
 #include <sys/socket.h>
 #include <unistd.h>
+#include <signal.h> // need for signal cmd
+#include <cerrno>
+#include <cstdio>
 
 int main()
 {
+	signal(SIGPIPE, SIG_IGN);  // Ignore SIGPIPE globally
 	// creating socket
 	int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
 
@@ -18,25 +22,46 @@ int main()
 	serverAddress.sin_addr.s_addr = INADDR_ANY;
 
 	// sending connection request
-	connect(clientSocket, (struct sockaddr*)&serverAddress, // connect is counterpart to accept
-			sizeof(serverAddress));
+	if (connect(clientSocket, (struct sockaddr*)&serverAddress, // connect is counterpart to accept
+			sizeof(serverAddress)) < 0)
+	{
+		perror("connect failed");
+		close(clientSocket);
+		return 1;
+	}
+	std::cout << "Connected to server! Type messages to send.\n";
 
 	// sending data
 	// const char* message = "Hello, server!";
 	// const char* message = ;
-	std::cout << "Connected to server! Type messages to send.\n";
-	std::cout << "(Type '*' to start sending message to server)\n";
-	std::cout << "(Type '#' to quit)\n";
+
 	std::string message;
 	while (true)
 	{
 		std::cout << "> ";
-		std::getline(std::cin, message);
-		send(clientSocket, message.c_str(), message.size(), 0);
-		if (message.find('#') != std::string::npos)
+		std::getline(std::cin, message); // \n gets striped from string
+
+		message += "\r\n";
+		std::cout << "[debug] sending message bytes: "; // d stands for \r and a for \n
+		for (size_t i = 0; i < message.size(); ++i)
+			std::cout << std::hex << (int)(unsigned char)message[i] << ' ';
+		std::cout << std::dec << std::endl;
+
+
+		ssize_t sent = send(clientSocket, message.c_str(), message.size(), 0);
+		if (sent == -1)
+		{
+			if (errno == EPIPE)
+				std::cout << "Server closed connection (EPIPE).\n";
+			else
+				perror("Error in sending message");
+			break;
+		}
+		if (message == "quit\r\n")
 			break;
 	}
 	// closing socket
+	std::cout << "Closing client connection.\n";
 	close(clientSocket);
 
 	return 0;
--- server/server.cpp
+++ server/server.cpp
@@ -4,6 +4,8 @@
 #include <iostream>
 #include <netinet/in.h> // needed for sockaddr_in
 #include <sys/socket.h> // for portability
+#include <sys/types.h>
+#include <arpa/inet.h> // for inet_pton
 #include <unistd.h> // for close
 #include <cstdio> // for perror
 #include <limits>
@@ -22,19 +24,29 @@ int main()
 {
 	int portnbr = 8080;
 	// creating server socket
-	int serverSocket = socket(AF_INET, SOCK_STREAM, 0); // input: IPv4 protocol, TCP socket, default
+	int serverSocket = socket(AF_INET, SOCK_STREAM, 0); // input: IPv4 protocol, TCP socket, default , also returns a fd
+	if (serverSocket == -1)
+	{
+		std::cerr << "Socket creation failed." << std::endl;
+		return -1;
+	}
 
 	// specifying the address
-	sockaddr_in serverAddress;
+	sockaddr_in serverAddress; // sockaddr_in is ip address vs4?
 	memset(&serverAddress, 0, sizeof(serverAddress)); // just nice to do not necessary
 	serverAddress.sin_family = AF_INET;
 	serverAddress.sin_port = htons(portnbr); // converts to network byte order
 	serverAddress.sin_addr.s_addr = INADDR_ANY; // accept connections on any IP
+	// inet_pton(AF_INET, "0.0.0.0", &serverAddress.sin_addr); // works similar to INADDR_ANY
+
+	// reuse port when server gets started to fast
+	int opt = 1;
+	setsockopt(serverSocket, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
 
 	// binding socket to address
-	bind(serverSocket, (struct sockaddr*)&serverAddress,
+	bind(serverSocket, (struct sockaddr *) &serverAddress,
 	     sizeof(serverAddress));
-
+	// todo add error handling to bind
 	// listening to the assigned socket
 	listen(serverSocket, 5); // n = how many pending connections can be queued
 	std::cout << "Server listening on port " << portnbr << "..." << std::endl;
@@ -42,50 +54,57 @@ int main()
 	sockaddr_in clientAddress;
 	socklen_t clientAddressLen = sizeof(clientAddress);
 	// accepting client connection
-	int clientSocket = accept(serverSocket, (struct sockaddr*)&clientAddress, &clientAddressLen);
+	int clientSocket = accept(serverSocket, (struct sockaddr *) &clientAddress, &clientAddressLen);
 	// accepts ands you a new socket FD representing one side of an already-established TCP connection.
-
+	// todo add error handling to accept
 	if (clientSocket < 0)
 	{
 		perror("accept failed");
 		exit(EXIT_FAILURE);
 	}
-
-	bool sessionActive = true;
-	char buffer[1024];
-	while (sessionActive) // means here is an accepted connection
+	std::cout << "Client connected." << std::endl;
+	std::string buffer;
+	char recvBuf[512];
+	//
+	while (true)
 	{
-		memset(buffer, 0, sizeof(buffer));
-		// receiving data from client
-		int bytesReceived = recv(clientSocket, buffer, sizeof(buffer) - 1, 0);
+		int bytesReceived = recv(clientSocket, recvBuf, sizeof(recvBuf) - 1, 0);
 		if (bytesReceived <= 0)
 		{
+			std::cout << "bytes received: "<< bytesReceived << std::endl;
 			std::cout << "Client disconnected" << std::endl;
 			break;
 		}
-		for (int i = 0; i < bytesReceived; i++)
-		{
-			char c = buffer[i];
-			if (c == '*')
-			{
-				std::cout << "Client can start to send message" << std::endl;
-				std::cout << "bytesReceived: " << bytesReceived << std::endl;
-				break;
-			}
 
-			else if (c == '#')
-			{
-				std::cout << "Client wants to end the connection" << std::endl;
-				sessionActive = false;
-				break;
-			}
-			// else
-			// 	std::cout << c;
+		recvBuf[bytesReceived] = '\0';
+		buffer += recvBuf;
+
+		size_t end;
+
+		// print buffer before filtering
+		// std::cout << "unfiltered buffer (escaped): ";
+		// for (size_t i = 0; i < buffer.size(); ++i)
+		// {
+		// 	unsigned char c = buffer[i];
+		// 	if (isprint(c))
+		// 		std::cout << c;
+		// 	else
+		// 		std::cout << "\\x" << std::hex << (int)c << std::dec;
+		// }
+		// std::cout << std::endl;
+
+		while ((end = buffer.find("\r\n")) != std::string::npos)
+		{
+
+			std::string message = buffer.substr(0, end);
+			buffer.erase(0, end + 2);
+			std::cout << "Cleaned message: " << message << std::endl;
+			// handleIRCCommand(message);  // parse and respond
 		}
-		std::cout << "bytesReceived: " << bytesReceived << std::endl;
-		std::cout << buffer << std::endl;
 	}
+
 	close(clientSocket);
 	close(serverSocket);
+	std::cout << "closing server connection" << std::endl;
 	return 0;
 }
